dcd8d305121356fde193af86f8fcf0cc00a43ab2
NatashaSenah <tashsenah@gmail.com>
2018-09-27 19:17:29 +0300

    add the created folder and file

--- album/views.py
+++ album/views.py
@@ -1,9 +1,9 @@
 import datetime as dt
 from django.http  import HttpResponse,Http404
-
+from django.shortcuts import render
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to My Gallery')
+    return render(request, 'welcome.html')
 def album_of_day(request):
     date = dt.date.today()
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
@@ -11,7 +11,7 @@ def album_of_day(request):
     html = f'''
         <html>
             <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                <h1>Album for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
