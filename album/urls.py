266fc9e349b25d91de6843eeca1fba88732f9180
NatashaSenah <tashsenah@gmail.com>
2018-09-29 13:49:40 +0300

    add the modified album

--- album/urls.py
+++ album/urls.py
@@ -5,8 +5,7 @@ from . import views
 
 urlpatterns=[
     url(r'^image/(\d+)',views.image,name ='image'),
-    url(r'^$',views.album_today,name='albumToday'),
-    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_album,name = 'pastAlbum'), 
+    url(r'^$',views.album_today,name='albumToday'), 
     url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
